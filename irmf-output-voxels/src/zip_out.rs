8ab5a94b6e08e5e175d5ad467b5a09160aa9ea3b
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2026-01-21 20:25:12 -0500

    Fix ZIP timestamps
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- irmf-output-voxels/src/zip_out.rs
+++ irmf-output-voxels/src/zip_out.rs
@@ -1,8 +1,8 @@
 use irmf_slicer::{Slicer, Renderer, IrmfResult};
 use std::fs::File;
-use std::io::BufWriter;
 use zip::write::FileOptions;
 use image::ImageFormat;
+use chrono::{Datelike, Timelike, Local};
 
 pub fn slice_to_zip<R: Renderer>(slicer: &mut Slicer<R>, material_num: usize, filename: &str) -> IrmfResult<()> {
     let file = File::create(filename).map_err(|e| anyhow::anyhow!("File::create: {}", e))?;
@@ -11,9 +11,21 @@ pub fn slice_to_zip<R: Renderer>(slicer: &mut Slicer<R>, material_num: usize, fi
     println!("Rendering Z-slices for ZIP...");
     slicer.prepare_render_z().map_err(|e| anyhow::anyhow!("prepare_render_z: {}", e))?;
 
+    let now = Local::now();
+    let dt = zip::DateTime::from_date_and_time(
+        now.year() as u16,
+        now.month() as u8,
+        now.day() as u8,
+        now.hour() as u8,
+        now.minute() as u8,
+        now.second() as u8,
+    ).map_err(|_| anyhow::anyhow!("Invalid current local time for ZIP"))?;
+    
+    let options = FileOptions::<()>::default().last_modified_time(dt);
+
     slicer.render_z_slices(material_num, |z_idx, _z, _radius, img| {
         let slice_name = format!("{:04}.png", z_idx);
-        zip.start_file(slice_name, FileOptions::<()>::default())
+        zip.start_file(slice_name, options)
             .map_err(|e| anyhow::anyhow!("zip.start_file: {}", e))?;
         
         let mut buffer = Vec::new();
